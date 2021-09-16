98ca9981c0804e3b3e440a00a7aa8fe8bea59f58
Julian Dumebi Duru <dduru@teamaptng.com>
2021-09-16 23:28:22 +0100

    - Updated Heroku Deployment

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
-app.listen(port);
+import history from 'connect-history-api-fallback';
+import express from 'express';
+
+var app = express();
+app.use(history());
+
 
