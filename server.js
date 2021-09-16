7d05a94264b64f0db5eb9a9f292c8d802d2aece0
Julian Dumebi Duru <dduru@teamaptng.com>
2021-09-16 23:48:20 +0100

    - ...

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ var express = require('express');
 var app = express();
 app.use(history());
 
+const port = process.env.PORT || 80;
 
+app.listen(port, () => {
+    console.log(`Socialotto UI app listening on port ${port}!!!`);
+});
