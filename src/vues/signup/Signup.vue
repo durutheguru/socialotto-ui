4cb2ae87e47312dbeb8a0362256ecc88e2d5c94e
kaine1 <kaine@onboardbase.com>
2021-10-05 15:27:14 +0100

    signup header

--- src/vues/signup/Signup.vue
+++ src/vues/signup/Signup.vue
@@ -1,12 +1,40 @@
 <template>
   <!-- <SignupComponent /> -->
-  <div class="">
+  <div style="height: 100%">
     <div class="w-full h-full">
-      <div class="w-full h-2.5">
-        <span class="">Socialotto</span>
+      <div class="signupHeader">
+        <span class="signupLogo">Socialotto</span>
       </div>
     </div>
-
+  </div>
 </template>
 
 <script lang="ts"></script>
+<style scoped>
+.signupHeader {
+  width: 100%;
+  height: 84px;
+  left: 0px;
+  top: 0px;
+  /* background: #ffffff; */
+  box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.14);
+}
+
+.signupLogo {
+  position: relative;
+  width: 175px;
+  height: 40px;
+  left: 144px;
+  top: 22px;
+
+  font-family: "Spartan", sans-serif;
+  font-style: normal;
+  font-weight: 900;
+  font-size: 40px;
+  line-height: 100%;
+
+  letter-spacing: -0.14em;
+
+  color: #4691a6;
+}
+</style>
