5f52717f35ffcf1d8af831e774fb2280b4f603a5
kaine1 <kaine@onboardbase.com>
2021-11-13 20:41:13 +0100

    caarousel works but with issues sliding from the full page screen

--- src/vues/campaign/vues/campaignDetails/campaignDetailsCarousel.vue
+++ src/vues/campaign/vues/campaignDetails/campaignDetailsCarousel.vue
@@ -1,257 +1,104 @@
 <template>
-  <div class="">
-    <!-- <img
-      src="https://socialotto.s3.ca-central-1.amazonaws.com/uploads/default-img-md-bg.jpeg"
-      style="width:100%"
-    /> -->
-    <div class="slideshow-container">
-      <div class="mySlides fade">
-        <div class="numbertext">1 / 3</div>
-        <img
-          src="https://socialotto.s3.ca-central-1.amazonaws.com/uploads/default-img-md-bg.jpeg"
-          style="width:100%; height: 360px;"
+  <div class="h-full relative rounded-md">
+    <carousel-slides
+      v-for="(slide, index) in slides"
+      :key="index"
+      :index="index"
+      :visibleSlide="visibleSlide"
+      :direction="direction"
+    >
+      <!-- <img class="w-full" :src="slide" alt="" /> -->
+
+      <div
+        class="h-full bg-cover  bg-center rounded-md"
+        :style="{
+          'background-image': 'url(' + slide + ')',
+        }"
+      ></div>
+    </carousel-slides>
+    <button
+      @click="next"
+      class="absolute right-0  top-1/3 px-2 py-4 bg-transparent-black cursor-pointer  hover:text-white"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M9 5l7 7-7 7"
         />
-        <div class="text">Caption Text</div>
-      </div>
-
-      <div class="mySlides fade">
-        <div class="numbertext">2 / 3</div>
-        <img
-          src="https://socialotto.s3.ca-central-1.amazonaws.com/uploads/images.jpg"
-          style="width:100% height: 360px;"
+      </svg>
+    </button>
+    <button
+      @click="prev"
+      class="absolute left-0 top-1/3 px-2 py-4 bg-transparent-black cursor-pointer hover:text-white"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6 "
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M15 19l-7-7 7-7"
         />
-        <div class="text">Caption Two</div>
-      </div>
-
-      <div class="mySlides fade">
-        <div class="numbertext">3 / 3</div>
-        <img
-          src="https://socialotto.s3.ca-central-1.amazonaws.com/uploads/images.jpg"
-          style="width:100% height: 360px;"
-        />
-        <div class="text">Caption Three</div>
-      </div>
-
-      <a class="prev" @click="plusSlides(-1)">&#10094;</a>
-      <a class="next" @click="plusSlides(1)">&#10095;</a>
-    </div>
-    <br />
-
-    <div style="text-align:center">
-      <span class="dot" @click="currentSlide(1)"></span>
-      <span class="dot" @click="currentSlide(2)"></span>
-      <span class="dot" @click="currentSlide(3)"></span>
-    </div>
+      </svg>
+    </button>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import CarouselSlides from "./CarouselSlides.vue";
 
 @Component({
   name: "CampaignDetailsCarousel",
+  components: {
+    CarouselSlides,
+  },
 })
 export default class CampaignDetailsCarousel extends Vue {
-  private plusSlides: any;
-  private currentSlide: any;
-  private showSlides: any;
+  private slides = [
+    "https://picsum.photos/id/237/500/300",
+    "https://picsum.photos/id/238/500/300",
+    "https://picsum.photos/id/239/500/300",
+  ];
+  private visibleSlide = 0;
 
-  private mounted() {
-    let slideIndex = 1;
+  private direction = "";
 
-    this.showSlides = (n: number) => {
-      let i;
-      let slides = document.getElementsByClassName(
-        "mySlides"
-      ) as HTMLCollectionOf<HTMLElement>;
-      let dots = document.getElementsByClassName("dot") as HTMLCollectionOf<
-        HTMLElement
-      >;
-      if (n > slides.length) {
-        slideIndex = 1;
-      }
-      if (n < 1) {
-        slideIndex = slides.length;
-      }
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
-      for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-      }
-      slides[slideIndex - 1].style.display = "block";
-
-      dots[slideIndex - 1].className += " active";
-    };
-
-    this.showSlides(slideIndex);
-
-    this.plusSlides = (n: number) => {
-      this.showSlides((slideIndex += n));
-    };
-
-    this.currentSlide = (n: number) => {
-      this.showSlides((slideIndex = n));
-    };
+  private next() {
+    this.direction = "left";
+    if (this.visibleSlide >= this.slides.length - 1) {
+      this.visibleSlide = 0;
+    } else {
+      this.visibleSlide++;
+    }
   }
-  // private slideIndex: number = 1;
-  // // private self = this;
-  // private showSlides(n: number) {
-  //   let i;
-  //   let slides = document.getElementsByClassName(
-  //     "mySlides"
-  //   ) as HTMLCollectionOf<HTMLElement>;
-  //   let dots = document.getElementsByClassName("dot") as HTMLCollectionOf<
-  //     HTMLElement
-  //   >;
-  //   if (n > slides.length) {
-  //     this.slideIndex = 1;
-  //   }
-  //   if (n < 1) {
-  //     this.slideIndex = slides.length;
-  //   }
-  //   for (i = 0; i < slides.length; i++) {
-  //     slides[i].style.display = "none";
-  //   }
-  //   for (i = 0; i < dots.length; i++) {
-  //     dots[i].className = dots[i].className.replace(" active", "");
-  //   }
-  //   slides[this.slideIndex - 1].style.display = "block";
-  //   dots[this.slideIndex - 1].className += " active";
-  // }
 
-  // this.showSlides(this.slideIndex);
-
-  // private plusSlides(n: number) {
-  //   this.showSlides(this.slideIndex += n);
-  // }
-
-  // private currentSlide(n) {
-  //   this.showSlides(this.slideIndex = n);
-  // }
+  private prev() {
+    this.direction = "right";
+    if (this.visibleSlide <= 0) {
+      this.visibleSlide = this.slides.length - 1;
+    } else {
+      this.visibleSlide--;
+    }
+  }
 }
 </script>
 
 <style scoped>
-* {
+/* * {
   box-sizing: border-box;
-}
-body {
-  font-family: Verdana, sans-serif;
-  margin: 0;
-}
-.mySlides {
-  display: none;
-}
-img {
-  vertical-align: middle;
-}
-
-/* Slideshow container */
-.slideshow-container {
-  max-width: 1000px;
-  position: relative;
-  margin: auto;
-}
-
-/* Next & previous buttons */
-.prev,
-.next {
-  cursor: pointer;
-  position: absolute;
-  top: 50%;
-  width: auto;
-  padding: 16px;
-  margin-top: -22px;
-  color: white;
-  font-weight: bold;
-  font-size: 18px;
-  transition: 0.6s ease;
-  border-radius: 0 3px 3px 0;
-  user-select: none;
-}
-
-/* Position the "next button" to the right */
-.next {
-  right: 0;
-  border-radius: 3px 0 0 3px;
-}
-
-/* On hover, add a black background color with a little bit see-through */
-.prev:hover,
-.next:hover {
-  background-color: rgba(0, 0, 0, 0.8);
-}
-
-/* Caption text */
-.text {
-  color: #f2f2f2;
-  font-size: 15px;
-  padding: 8px 12px;
-  position: absolute;
-  bottom: 8px;
-  width: 100%;
-  text-align: center;
-}
-
-/* Number text (1/3 etc) */
-.numbertext {
-  color: #f2f2f2;
-  font-size: 12px;
-  padding: 8px 12px;
-  position: absolute;
-  top: 0;
-}
-
-/* The dots/bullets/indicators */
-.dot {
-  cursor: pointer;
-  height: 15px;
-  width: 15px;
-  margin: 0 2px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
-  transition: background-color 0.6s ease;
-}
-
-.active,
-.dot:hover {
-  background-color: #717171;
-}
-
-/* Fading animation */
-.fade {
-  -webkit-animation-name: fade;
-  -webkit-animation-duration: 1.5s;
-  animation-name: fade;
-  animation-duration: 1.5s;
-}
-
-@-webkit-keyframes fade {
-  from {
-    opacity: 0.4;
-  }
-  to {
-    opacity: 1;
-  }
-}
-
-@keyframes fade {
-  from {
-    opacity: 0.4;
-  }
-  to {
-    opacity: 1;
-  }
-}
-
-/* On smaller screens, decrease text size */
-@media only screen and (max-width: 300px) {
-  .prev,
-  .next,
-  .text {
-    font-size: 11px;
-  }
-}
+} */
 </style>
