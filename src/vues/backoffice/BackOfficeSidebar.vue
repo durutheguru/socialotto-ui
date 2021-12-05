6540d1f7668576125f24b6f966b85ddd41fa46ce
kaine1 <kaine@onboardbase.com>
2021-12-05 01:27:29 +0100

    lottery owner filter

---
+++ src/vues/backoffice/BackOfficeSidebar.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="col-span-1 w-full h-full">
+    <div class="flex-1 flex flex-col h-full backOfficeSidebar">
+      <div class="flex-1 flex flex-col pt-16 pb-4 overflow-y-auto w-56 mx-auto">
+        <div class="">
+          <div
+            style="background: #2B606E;"
+            class="flex flex-col  px-4 mx-auto items-center rounded-lg py-6 w-full"
+          >
+            <!-- --- -->
+            <div class="w-full text-center">
+              <img
+                class="inline-block h-24 w-24 rounded-full"
+                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt=""
+              />
+            </div>
+            <!-- ---------- -->
+            <div class="w-full text-center">
+              <h1 class="mt-2 text-2xl text-white spartan font-medium">
+                Dumebi Duru
+              </h1>
+              <h2 class="text-lg text-white spartan font-normal">Admin</h2>
+            </div>
+            <!-- ---------- -->
+            <div class="w-full flex justify-center">
+              <div class="mr-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6 text-white"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  />
+                </svg>
+              </div>
+              <div>
+                <svg
+                  class="h-6 w-6"
+                  viewBox="0 0 14 14"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M10.71 3.56641C11.444 4.3007 11.9439 5.23616 12.1463 6.25452C12.3488 7.27287 12.2447 8.32838 11.8473 9.2876C11.4499 10.2468 10.777 11.0666 9.91364 11.6434C9.05032 12.2202 8.03535 12.5281 6.99707 12.5281C5.95879 12.5281 4.94383 12.2202 4.08051 11.6434C3.21719 11.0666 2.54428 10.2468 2.14687 9.2876C1.74946 8.32838 1.64539 7.27287 1.84782 6.25452C2.05026 5.23616 2.5501 4.3007 3.28416 3.56641"
+                    stroke="white"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M7 1.56641V7.39974"
+                    stroke="white"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- -------Nav-------- -->
+        <nav class="mt-5 flex-1  space-y-1 spartan" aria-label="Sidebar">
+          <router-link
+            v-for="item in navigation"
+            :key="item.name"
+            :to="`/back-office/${item.href}`"
+            class=" font-normal
+                text-gray-300  hover:text-white
+              group flex items-center  py-3 text-md
+              hover:font-bold "
+          >
+            <span class="flex-1">
+              {{ item.name }}
+            </span>
+          </router-link>
+        </nav>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "BackofficeSidebar",
+})
+export default class BackofficeSidebar extends Vue {
+  private navigation = [
+    { name: "Dashboard", href: "#", current: true },
+    {
+      name: "Create A Lottery",
+      href: "create-lottery",
+      count: 3,
+      current: false,
+    },
+    { name: "Campaign Requests", href: "#", count: 4, current: false },
+    { name: "Raise Lottery Expense", href: "#", current: false },
+    { name: "Manage Users", href: "#", current: false },
+    { name: "Approval Requests", href: "#", count: 12, current: false },
+  ];
+}
+</script>
+
+<style scoped>
+a.router-link-active {
+  color: white;
+  font-weight: 600;
+}
+</style>
---
+++ src/vues/backoffice/vues/lottery/CreateLottery.vue
@@ -0,0 +1,250 @@
+<template>
+  <div class="col-span-5 pt-20 px-10">
+    <h1
+      class="flex justify-center sm:justify-start spartan text-3xl font-semibold text-black mb-6"
+    >
+      Create your campaign
+    </h1>
+    <validation-observer
+      ref="observer"
+      id="createLottery_form"
+      tag="form"
+      role="form"
+      v-slot="{ invalid }"
+      @submit.prevent="createLottery"
+      novalidate
+    >
+      <div class=" grid grid-cols-6 ">
+        <div class="col-span-3">
+          <div class="w-11/12">
+            <div class="w-full">
+              <div class="w-full mb-6">
+                <label
+                  for="Lottery_title"
+                  class="spartan font-medium text-dark block text-sm text-gray-700"
+                  >Lottery title</label
+                >
+                <div class="mt-1">
+                  <validation-provider
+                    mode="aggressive"
+                    rules="required|min:5"
+                    v-slot="{ errors }"
+                  >
+                    <input
+                      v-model="lottery.name"
+                      required
+                      type="text"
+                      name="lottery title"
+                      id="campaign_title"
+                      :class="{
+                        'border-red-400': errors.length > 0,
+                      }"
+                      class="spartan h-12 bg-transparent  border-gray-300 border-2  px-2  focus:border-blue-500 block w-full sm:text-sm rounded-md"
+                      placeholder="title"
+                    />
+                    <span class="text-red-500 spartan">{{ errors[0] }}</span>
+                  </validation-provider>
+                </div>
+              </div>
+
+              <!-- -----------lottery description----------- -->
+              <div class="w-full mb-6">
+                <label
+                  for="Lottery_description"
+                  class="spartan font-medium text-dark block text-sm text-gray-700"
+                  >Lottery description</label
+                >
+                <div class="mt-1">
+                  <validation-provider
+                    mode="aggressive"
+                    rules="required|min:20|max:240"
+                    v-slot="{ errors }"
+                  >
+                    <textarea
+                      v-model="lottery.description"
+                      autocomplete="off"
+                      id="lottery description"
+                      required
+                      rows="6"
+                      :class="{
+                        'border-red-400': errors.length > 0,
+                      }"
+                      class="max-h-44 spartan bg-transparent rounded-md px-2 pt-2 pb-8 form-textarea mt-1 border-gray-300 border-2 block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5  focus:border-blue-500"
+                      placeholder="description"
+                    ></textarea>
+                    <span class="text-red-500 spartan">{{ errors[0] }}</span>
+                  </validation-provider>
+                </div>
+              </div>
+
+              <!-- -------Lottery Owner------- -->
+              <div class="w-full mb-6 relative">
+                <label
+                  for="Lottery owner"
+                  class="spartan font-medium text-dark block text-sm font-medium text-gray-700"
+                  >Lottery owner</label
+                >
+                <div class="mt-1">
+                  <validation-provider rules="required" v-slot="{ errors }">
+                    <input
+                      v-model="lottery.lotteryOwner"
+                      required
+                      type="text"
+                      name="Lottery owner"
+                      id="lottery owner"
+                      :class="{
+                        'border-red-400': errors.length > 0,
+                      }"
+                      class="spartan h-12 bg-transparent  border-gray-300 border-2  px-2 focus:ring-indigo-500 focus:border-blue-500 block w-full sm:text-sm rounded-md"
+                      placeholder="Search"
+                    />
+
+                    <span class="text-red-500 spartan">{{ errors[0] }}</span>
+                  </validation-provider>
+                </div>
+                <ul class="absolute" v-if="lottery.lotteryOwner.length > 0">
+                  <li v-for="owner in owners" :key="owner">{{ owner }}</li>
+                </ul>
+              </div>
+              <!-- ------------- -->
+            </div>
+          </div>
+        </div>
+
+        <!-- --------------------------Second half of page inputs------------------------------ -->
+        <div class="col-span-3">
+          <div class="w-11/12">
+            <!-- -----Cost per ticket---- -->
+            <div class="w-full mb-6">
+              <label
+                for="Cost per ticket"
+                class="spartan font-medium text-dark block text-sm font-medium text-gray-700"
+                >Cost per ticket</label
+              >
+              <div class="mt-1">
+                <validation-provider
+                  rules="required|numeric"
+                  v-slot="{ errors }"
+                >
+                  <input
+                    v-model="lottery.ticketCost"
+                    required
+                    min="200"
+                    oninput="this.value = Math.abs(Math.round(this.value));"
+                    type="number"
+                    name="ticket cost"
+                    id="ticket cost"
+                    :class="{
+                      'border-red-400': errors.length > 0,
+                    }"
+                    class="spartan h-12 bg-transparent  border-gray-300 border-2  px-2 focus:ring-indigo-500 focus:border-blue-500 block w-full sm:text-sm rounded-md"
+                    placeholder="N200"
+                  />
+
+                  <span class="text-red-500 spartan">{{ errors[0] }}</span>
+                </validation-provider>
+              </div>
+            </div>
+
+            <!-- ------Number of winners------ -->
+
+            <div class="w-full mb-6">
+              <label
+                for="Number of winners"
+                class="spartan font-medium text-dark block text-sm font-medium text-gray-700"
+                >Number of winners</label
+              >
+              <div class="mt-1">
+                <validation-provider
+                  rules="required|numeric"
+                  v-slot="{ errors }"
+                >
+                  <input
+                    v-model="lottery.numberOfWinners"
+                    required
+                    min="1"
+                    oninput="this.value = Math.abs(Math.round(this.value));"
+                    type="number"
+                    name="Number of winners"
+                    id="number of winners"
+                    :class="{
+                      'border-red-400': errors.length > 0,
+                    }"
+                    class="spartan h-12 bg-transparent  border-gray-300 border-2  px-2 focus:ring-indigo-500 focus:border-blue-500 block w-full sm:text-sm rounded-md"
+                    placeholder="25"
+                  />
+
+                  <span class="text-red-500 spartan">{{ errors[0] }}</span>
+                </validation-provider>
+              </div>
+            </div>
+
+            <!-- ---------- -->
+            <button
+              :disabled="invalid"
+              :class="[invalid ? 'opacity-25' : 'opacity-100']"
+              class="bg-blue-200 spartan w-full flex justify-center items-center h-12 px-4 rounded-md text-white focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            >
+              Create lottery
+              <!-- <i
+              class="ml-px fa fa-spinner fa-spin"
+              v-if="saveCampaign.loading"
+            ></i> -->
+            </button>
+          </div>
+        </div>
+        <!-- ------------------------------------ -->
+      </div>
+    </validation-observer>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Watch } from "vue-property-decorator";
+import ApiResource from "@/components/core/ApiResource";
+import { Log, Constants, Util } from "@/components/util";
+
+@Component({
+  name: "CreateLottery",
+})
+export default class CreateLottery extends Vue {
+  private mounted() {
+    // this.populateOwnersArray();
+  }
+  private owners: any = [];
+
+  private populateOwnersArray() {
+    const ownersList = ["dumebi", "kaine"];
+    this.owners = ownersList.filter((owner) => {
+      return owner.match(this.getOwner);
+    });
+
+    Log.info("filteredOwners: " + this.owners + " " + this.getOwner);
+  }
+
+  private get getOwner() {
+    return this.lottery.lotteryOwner;
+  }
+
+  @Watch("getOwner")
+  private ownersFilter(newValue: string, oldValue: string) {
+    this.populateOwnersArray();
+  }
+
+  private saveLottery: ApiResource = ApiResource.create();
+
+  private lottery: any = {
+    name: "",
+    description: "",
+    ticketCost: "",
+    numberOfWinners: "",
+    lotteryOwner: "",
+  };
+
+  private createLottery() {
+    Log.info("lotteryDetails: " + JSON.stringify(this.lottery));
+  }
+}
+</script>
+
+<style scoped></style>
