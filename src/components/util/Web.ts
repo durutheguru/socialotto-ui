8688d837965865305336554bf84293eabbe3fc0a
Julian Duru <durutheguru@gmail.com>
2022-05-07 17:14:32 +0100

    - Removed Server Sent Events integration

--- src/components/util/Web.ts
+++ src/components/util/Web.ts
@@ -23,12 +23,13 @@ axios.interceptors.request.use((config: AxiosRequestConfig): any => {
 
 // Add a response interceptor
 axios.interceptors.response.use(
-  function(response) {
+  (response) => {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     return response;
   },
-  function(error) {
+  
+  (error) => {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     if (error.response.status === 401 || error.response.status === 403) {
